0f9e87c477f375a3985c17a3f7a357d7b5556fc6
Iman Jawad <imanj12@gmail.com>
2018-07-16 14:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var recipe = {}
+var ingredient
+var amount
 
-function updateObjectWithKeyAndValue(recipe, )
+function updateObjectWithKeyAndValue(recipe, ingredient, amount) {
+  Object.assign({}, recipe, { [ingredient]: amount })
+  return recipe
+}
